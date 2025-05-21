4de6dea5ccbde91980f38b1b53b41bf879985860
LauraValdez3 <laura03amy@gmail.com>
2025-05-20 19:39:18 -0700

    Editar, eliminar y actualizar

--- app/src/main/java/mx/itson/cheemstour/TripListActivity.kt
+++ app/src/main/java/mx/itson/cheemstour/TripListActivity.kt
@@ -1,8 +1,10 @@
 package mx.itson.cheemstour
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import mx.itson.cheemstour.adapters.TripAdapter
 import mx.itson.cheemstour.entities.Trip
@@ -48,6 +50,38 @@ class TripListActivity : AppCompatActivity() {
                 Log.e("TripListActivity", "Error en la API: ${t.message}")
             }
         })
+    }
+
+    fun deleteTrip(tripId: Int) {
+        val context = this
+        val call: Call<Boolean> = RetrofitUtil.getApi()!!.deleteTrip(tripId)
+        call.enqueue(object : Callback<Boolean> {
+            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
+                if (response.isSuccessful) {
+                    val isDeleted = response.body() ?: false
+                    if (isDeleted) {
+                        Toast.makeText(context, getString(R.string.text_deleted_successful), Toast.LENGTH_LONG).show()
+                        // Redirigir o refrescar la lista
+                        val intent = Intent(context, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(context, getString(R.string.text_deleted_error), Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    Log.e("Error", "Failed to delete trip: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                Log.e("Error", "Error calling API: ${t.message}")
+            }
+        })
+    }
+
+    fun editTrip(trip: Trip) {
+        val intent = Intent(this, EditTripActivity::class.java)
+        intent.putExtra("trip", trip)
+        startActivity(intent)
     }
 
 
